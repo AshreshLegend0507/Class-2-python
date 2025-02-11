a71d76f5a2f7d1dda8ee2b1c78e3933a858735d5
AshreshLegend0507 <ashreshreddyp2013@gmail.com>
2025-02-11 19:50:26 +0530

    Add files via upload

---
+++ Lesson 1/main.py
@@ -0,0 +1,16 @@
+print("Welcome to the world of programming")
+
+print(4)
+
+print("Hello Python \n")
+
+print("hello",5)
+
+print("welcome to \n ", end= "thank you 123 ")
+
+print()
+
+print("My name is Ashresh \nI am 12 years old")
+
+
+
---
+++ Lesson 1/main2.py
@@ -0,0 +1,8 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+Codingal = "Codingal"
+print(Codingal)
+name = input("enter your name: ")
+print("My name is",name)
